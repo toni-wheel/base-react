3ddf9b2387fce2658ab67a8222dce795d3d97822
anton <toni_wheel@inbox.ru>
2023-08-30 16:39:25 +0300

    Старт

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles/reset.scss";
+import "./styles/add.scss";
+import "./styles/fonts.scss";
+import "./styles/base.scss";
+
+function App() {
+  return <div className="App">Мое первое приложение на React</div>;
+}
+
+export default App;
